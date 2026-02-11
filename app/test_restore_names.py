287453c7983198e9851975d1050963aa4165c8ba
Your Name <allanaumenko@Allas-MacBook-Air.local>
2026-02-11 21:27:10 +0800

    restore_names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,93 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+from app.main import restore_names
+
+
+def test_restore_first_name_when_none() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+    ]
+
+    restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+    ]
+
+
+def test_restore_first_name_when_missing() -> None:
+    users = [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_do_not_change_existing_first_name() -> None:
+    users = [
+        {
+            "first_name": "Anna",
+            "last_name": "Brown",
+            "full_name": "Anna Brown",
+        },
+    ]
+
+    restore_names(users)
+
+    assert users[0]["first_name"] == "Anna"
+
+
+def test_restore_names_for_multiple_users() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_empty_list() -> None:
+    users = []
+    restore_names(users)
+    assert users == []
+
